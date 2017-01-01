f3cf873e70c249d3a3ff00eff35ae6f94c95b86c
NikolayRag <nikolayr@mail.ru>
2017-01-01 20:56:52 +0300

    clean description

--- __init__.py
+++ __init__.py
@@ -1,3 +1,35 @@
+'''
+Lightweighted version of Yi4k API, reverse-engineered from official Java API.
+It is limited so:
+	- values provided to commands should be correct strings,
+	- camera data exchanging operations are blocking,
+	- no callbacks are supported.
+
+Commands not implemented:
+
+	formatSDCard
+		NSFW
+
+	deleteFile
+		Considered vulnerable, maybe later
+
+	downloadFile
+	cancelDownload
+		Redundant, available by http
+
+	getRtspURL
+		Redundant, available at YiAPI() creation
+
+	buildLiveVideoQRCode
+		Maybe later
+
+	startRecording datetime
+		Lazy to implement
+
+'''
+
+
+
 from .yiClasses import *
 
 
--- yiClasses.py
+++ yiClasses.py
@@ -5,38 +5,6 @@ from kiLog import *
 
 
 
-'''
-Lightweighted version of Yi4k API, reverse-engineered from official Java API.
-It is limited so:
-	- values provided to commands should be correct strings,
-	- camera data exchanging operations are blocking,
-	- no callbacks are supported.
-
-Commands not implemented:
-
-	formatSDCard
-		NSFW
-
-	deleteFile
-		Considered vulnerable, maybe later
-
-	downloadFile
-	cancelDownload
-		Redundant, available by http
-
-	getRtspURL
-		Redundant, available at YiAPI() creation
-
-	buildLiveVideoQRCode
-		Maybe later
-
-	startRecording datetime
-		Lazy to implement
-
-'''
-
-
-
 commands= []
 
 
